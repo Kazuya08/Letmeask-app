5a80556c9d3b37cffc7f6606cb479a118a1e3aee
Kazuya08 <kazuyathiago@hotmail.com>
2021-06-22 22:35:36 -0300

    Aula 02 - Maximum Speed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
+import { ButtonHTMLAttributes } from "react";
 
-export function Button() {
-  //   let counter = 0;
+import "../styles/button.scss";
 
-  const [counter, setCounter] = useState(0);
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-  function increment() {
-    // counter += 1;
-    setCounter(counter + 1);
-  }
-
-  return <button onClick={increment}>{counter}</button>;
+export function Button(props: ButtonProps) {
+  return <button className="button" {...props}></button>;
 }
